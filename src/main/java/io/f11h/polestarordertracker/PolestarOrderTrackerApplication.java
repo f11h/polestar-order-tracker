e4d9c7fdc0c2efde5d3668af8f38ff6a48f0a14d
Felix Dittrich <felix@fd-wob.de>
2023-01-27 22:36:22 +0100

    Initial Project Setup

---
+++ src/main/java/io/f11h/polestarordertracker/PolestarOrderTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.f11h.polestarordertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolestarOrderTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolestarOrderTrackerApplication.class, args);
+    }
+
+}
